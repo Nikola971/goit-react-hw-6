ea6dcf3cd70d4b30acf4b4d37eac5f6cb762aca6
Nikola971 <mukola.milyan@outlook.com>
2024-02-22 10:39:14 +0200

    кап

--- src/componentes/ContactList/ContactList.jsx
+++ src/componentes/ContactList/ContactList.jsx
@@ -1,18 +1,24 @@
+
+import React from 'react';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 import { useSelector } from 'react-redux';
+import * as selectors from '../../redux/selectors';
 
 export default function ContactList() {
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
-  const filters = contacts.filter(contact => {
+  const contacts = useSelector(selectors.selectContacts);
+  const filter = useSelector(selectors.selectFilter);
+
+  const filteredContacts = contacts.filter(contact => {
     return contact.name.toLowerCase().includes(filter.toLowerCase());
   });
-  console.log(contacts);
+
+  console.log(filteredContacts);
+
   return (
     <div>
       <ul className={css.container}>
-        {filters.map(item => {
+        {filteredContacts.map(item => {
           return (
             <li key={item.id} className={css.item}>
               <Contact contacts={item} />
